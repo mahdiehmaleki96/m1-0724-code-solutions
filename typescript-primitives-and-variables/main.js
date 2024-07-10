2c4695fe790f90039935c72fc7b20aa908056f7e
Mahdieh Maleki <mahdiehmaleki96@gmail.com>
2024-07-10 02:17:43 +0000

    completed typescript-primitives-and-variables

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+'use strict';
+const fullName = 'Mahdieh Maleki';
+const isCool = true;
+const totalPets = 1;
+const empty = null;
+let nothing;
+console.log('value of fullName is: ', fullName);
+console.log('value of isCool is: ', isCool);
+console.log('value of totalPets is: ', totalPets);
+console.log('type of fullName is: ', typeof fullName);
+console.log('type of isCool is: ', typeof isCool);
+console.log('type of totalPets is: ', typeof totalPets);
+console.log('value of empty is: ', empty);
+console.log('type of empty is: ', typeof empty);
+console.log('value of nothing is: ', nothing);
+console.log('type of nothing is: ', typeof nothing);
---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,16 @@
+const fullName: string = 'Mahdieh Maleki';
+const isCool: boolean = true;
+const totalPets: number = 1;
+const empty: null = null;
+let nothing: undefined;
+
+console.log('value of fullName is: ', fullName);
+console.log('value of isCool is: ', isCool);
+console.log('value of totalPets is: ', totalPets);
+console.log('type of fullName is: ', typeof fullName);
+console.log('type of isCool is: ', typeof isCool);
+console.log('type of totalPets is: ', typeof totalPets);
+console.log('value of empty is: ', empty);
+console.log('type of empty is: ', typeof empty);
+console.log('value of nothing is: ', nothing);
+console.log('type of nothing is: ', typeof nothing);
