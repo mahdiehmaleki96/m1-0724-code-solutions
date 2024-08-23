846498b03fd7add4a7e3b713624236f1a95ffab7
Mahdieh Maleki <mahdiehmaleki96@gmail.com>
2024-08-23 22:22:37 +0000

    completed dom-querying

---
+++ dom-querying/main.ts
@@ -0,0 +1,19 @@
+console.log('hello, world');
+
+const $elementHeading = document.querySelector('h1');
+console.log($elementHeading);
+console.dir($elementHeading);
+
+const $explanationId = document.querySelector('#explanation');
+console.log($explanationId);
+console.dir($explanationId);
+
+const $hintClass = document.querySelector('.hint');
+console.log($hintClass);
+console.dir($hintClass);
+
+const $elementPNodeList = document.querySelectorAll('p');
+console.log($elementPNodeList);
+
+const $exampleLinkClass = document.querySelectorAll('.example-link');
+console.log($exampleLinkClass);
