60a81bab3b1903fa7e63cf933f39468a5c1a7296
Mahdieh Maleki <mahdiehmaleki96@gmail.com>
2024-08-27 18:25:02 +0000

    completed dom-events

---
+++ dom-events/buttons.js
@@ -0,0 +1,33 @@
+'use strict';
+const $buttonElement = document.querySelector('.click-button');
+if (!$buttonElement) {
+  throw new Error('button does not exist');
+}
+function handleClick(event) {
+  console.log('button clicked');
+  console.log(event);
+  console.log(event.target);
+}
+$buttonElement.addEventListener('click', handleClick);
+const $buttonHoverElement = document.querySelector('.hover-button');
+if (!$buttonHoverElement) {
+  throw new Error('button hover element does not exist');
+}
+function handleMouseover(event) {
+  console.log('button hovered');
+  console.log(event);
+  console.log(event.target);
+}
+$buttonHoverElement.addEventListener('mouseover', handleMouseover);
+const $buttonDoubleClickElement = document.querySelector(
+  '.double-click-button'
+);
+if (!$buttonDoubleClickElement) {
+  throw new Error('double click button does not exist');
+}
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log(event);
+  console.log(event.target);
+}
+$buttonDoubleClickElement.addEventListener('dblclick', handleDoubleClick);
