af930270be3cf184c53aab8b9f0f301c552b1842
Mahdieh Maleki <mahdiehmaleki96@gmail.com>
2024-10-01 02:47:58 +0000

    completed practice-interview-challenge

---
+++ practice-interview-challenge/main.js
@@ -0,0 +1,25 @@
+'use strict';
+function getRangeReport(start, end) {
+  const range = [];
+  const odds = [];
+  const evens = [];
+  let total = 0;
+  for (let i = start; i <= end; i++) {
+    range.push(i);
+    total += i;
+    if (i % 2 == 0) {
+      evens.push(i);
+    } else {
+      odds.push(i);
+    }
+  }
+  const average = total / range.length;
+  return {
+    total,
+    odds,
+    evens,
+    range,
+    average,
+  };
+}
+console.log(getRangeReport(1, 10));
